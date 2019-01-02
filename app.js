3c2dd0feb4ef02226806ecd7f6c5ddb272a098fe
yodfhafx <yodfha2014@gmail.com>
2019-01-02 23:24:04 +0700

    send form with nodemailer

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-    res.render('index', {title: 'welcome'});
+    res.render('index', {title: 'welcome to my website'});
 });
 
 app.get('/about', function(req, res) {
-    res.render('about');
+    res.render('about', {title: 'about us'});
 });
 
 app.get('/contact', function(req, res) {
@@ -28,14 +28,28 @@ app.post('/contact/send', function(req, res) {
     var transporter = nodeMailer.createTransport({
         service: 'Gmail',
         auth: {
-            user: 'yodfha2014@gmail.com',
-            pass: ''
+            user: 'realgmail@gmail.com',
+            pass: 'realpassword'
         }
     });
 
     var mailOptions = {
-        from: 'Yodfha Thungwiwatthanakul'
-    }
+        from: 'somename <sender@gmail.com>',
+        to: 'receivermail@gmail.com',
+        subject: 'Express Website',
+        text: 'Submission details... Name: '+ req.body.name +'Enail: '+ req.body.email +'Message: '+ req.body.message,
+        html: '<p>Submission details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if(error) {
+            console.log(error);
+            res.redirect('/');
+        } else {
+            console.log('Message Sent: '+ info.response);
+            res.redirect('/');
+        }
+    });
 });
 
 app.listen(3000);
